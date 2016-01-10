9abd7488b2891fa51d0744d4ed1b4105c5b98817
Stijn Moreels <stijn-moreels@hotmail.com>
2016-01-10 10:25:39 +0100

    Create loadModel.js

---
+++ loadModel.js
@@ -0,0 +1,17 @@
+/* =============================================================================
+ * @project: Node.js Abstraction
+ * @author: Stijn Moreels
+ * @language: Node.js
+ * @purpose: Generic Middleware
+ =============================================================================*/
+ 
+ module.exports = (function (repository) {
+    return function (request, response, next) {
+        var id = request.params.id || request.body._id;
+        repository.getOne(parseInt(id), function (error, doc) {
+            if (error) { throw error; }
+            else request.model = doc;
+            next();
+        });
+    };
+});
